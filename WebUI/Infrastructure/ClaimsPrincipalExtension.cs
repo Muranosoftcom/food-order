bec76ab579f0a1580f69126ab7906f64895fa4cb
Sergey Olefir <senkaolefir@gmail.com>
2018-10-27 13:13:09 +0300

    New controllers admin

---
+++ WebUI/Infrastructure/ClaimsPrincipalExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebUI.Infrastructure {
+    public static class ClaimsPrincipalExtension {
+        public static bool IsAdmin(this ClaimsPrincipal claims)
+        {
+            return bool.TryParse(claims.Claims.FirstOrDefault(x => x.Type == "isAdmin")?.Value, out bool isAdmin) && isAdmin;
+        }
+    }
+}
