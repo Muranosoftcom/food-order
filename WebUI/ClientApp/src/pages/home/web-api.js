74a2c3da62ad2645570c506426e7eec45b17bfd2
ronatskiy <r.onatskiy@gmail.com>
2018-10-27 13:44:08 +0300

    save UI

--- WebUI/ClientApp/src/pages/home/web-api.js
+++ WebUI/ClientApp/src/pages/home/web-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_ENDPOINT = "/api/order/get-week-order";
+const API_ENDPOINT = "/api/order/get-today-order";
 
 export async function getWeekOrder() {
 	return (await axios.get(API_ENDPOINT)).data;
