79e9354f2efd4f005062e208642236ef8ce4bd0a
Egor Manevich <manevich.egor@gmail.com>
2018-10-27 12:10:36 +0300

    Add post-order

--- BusinessLogic/DTOs/CategoryDto.cs
+++ BusinessLogic/DTOs/CategoryDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessLogic.DTOs
 {
     public class CategoryDto
     {
+        [Required]
         public int Id { get; set; }
+        
         public string Name { get; set; }
+        [Required]
         public DishDto[] Dishes { get; set; }
     }
 }
--- BusinessLogic/DTOs/DishDto.cs
+++ BusinessLogic/DTOs/DishDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Core;
 
 namespace BusinessLogic.DTOs
 {
     public class DishDto
     {
+        [Required]
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
