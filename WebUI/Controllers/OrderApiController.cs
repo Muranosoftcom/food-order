702d8e2fad2c967f9bea11e8ba58790044bb014f
Egor Manevich <manevich.egor@gmail.com>
2018-10-27 13:13:29 +0300

    Fix get-week-menu

--- WebUI/Controllers/OrderApiController.cs
+++ WebUI/Controllers/OrderApiController.cs
@@ -122,14 +122,17 @@ namespace WebUI.Controllers
         {
             var now = DateTime.UtcNow;
 
-            var availableDishes = _repo.All<DishItem>().Include(x => x.AvailableOn)
-//                .Where(x => x.AvailableUntil >= now)
-                .ToArray();
+            var availableDishes = _repo.All<DishItem>()
+                .Include(x => x.AvailableOn)
+                .Include(x => x.Supplier)
+                .Include(x => x.Category)
+                .Where(x => x.AvailableUntil >= now);             
 
             List<(string dayName, DishItem dish)> dayNameDishPairs = new List<(string, DishItem)>();
             foreach (var dish in availableDishes)
             {
-                var dayNames = dish.AvailableOn.Select(x => x.WeekDay.Name).ToArray();
+                var dayIds = new HashSet<int>(dish.AvailableOn.Select(x => x.WeekDayId));
+                var dayNames = _repo.All<WeekDay>().Where(x => dayIds.Contains(x.Id)).Select(x => x.Name);
                 foreach (var dayName in dayNames)
                 {
                     dayNameDishPairs.Add((dayName: dayName, dish: dish));
@@ -145,22 +148,23 @@ namespace WebUI.Controllers
                     WeekDay = x.Key,
                     Suppliers = x.GroupBy(d => (id: d.Supplier.Id, name: d.Supplier.Name)).Select(d =>
                     {
-                        var dishItemByPair =
-                            d.ToDictionary(di => (categoryId: di.Category.Id, categoryName: di.Category.Name),
-                                di => di);
+                        var dishItemByPairPairs =
+                            d.Select(di => (key: (categoryId: di.Category.Id, categoryName: di.Category.Name),
+                                value: di));
+                        var dishItemByPair = dishItemByPairPairs.ToLookup(y => y.key, y => y.value);
                         return new SupplierDto
                         {
                             SupplierId = d.Key.id,
                             SupplierName = d.Key.name,
-                            Categories = d.Select(dishSupplierPair => new CategoryDto
+                            Categories = dishItemByPair.Select(z => new CategoryDto
                             {
-                                Id = dishSupplierPair.Category.Id,
-                                Name = dishSupplierPair.Category.Name,
-                                Dishes = dishItemByPair.Select(y => new DishDto
+                                Id = z.Key.categoryId,
+                                Name = z.Key.categoryName,
+                                Dishes = z.Select(f => new DishDto
                                 {
-                                    Id = y.Value.Id,
-                                    Name = y.Value.Name,
-                                    Price = y.Value.Price
+                                    Id = f.Id,
+                                    Name = f.Name,
+                                    Price = f.Price
                                 }).ToArray()
                             }).ToArray()
                         };
